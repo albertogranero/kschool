77d4641b446880284f4db3e5f3dc803b6d66211a
0xdevgranero <devgranero@proton.me>
2024-01-31 20:43:34 +0100

    verificar_palindromo.py arreglada

--- verificar_palindromo.py
+++ verificar_palindromo.py
@@ -1,8 +1,8 @@
 def verificar_palindromo(palabra):
     palabra = palabra.lower()
-    reverso = palabra[::-2]
-    
-    if palabra != reverso:
+    reverso = palabra[::-1]
+
+    if palabra == reverso:
         return True
     else:
         return False
